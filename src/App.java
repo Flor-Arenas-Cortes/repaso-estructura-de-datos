b55d6c738b7de08cee6bbf49d824a538b0d078fc
Flor Arenas <flarenas@correo.univo.edu.mx>
2025-08-16 15:35:15 -0600

    Se agrego el ejercicio de Array

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        // Array (Arreglo) - Estructura indexada de tamaño fijo
+        // Ejercicio de calificaciones
+
+        //Declaramos un arreglo de 5 calificaciones
+        double[] calificaciones = new double[5];
+        Scanner sc = new Scanner(System.in);
+        double suma = 0;
+
+        //Pedimos al usuario ingresas 5 calificaciones
+        for (int i = 0; i < calificaciones.length; i++){
+            System.out.println("Ingresa la calificación " + (i + 1) + ": ");
+            calificaciones [i] = sc.nextDouble(); // Guardamos las calificaciones
+            suma += calificaciones[i]; // Sumamos para calcular promedio después
+        }
+
+        //Calculamos el promedio
+        double promedio = suma / calificaciones.length;
+
+        //Mostrar el resultado
+        System.out.println("El promedio es: " + promedio);
     }
 }
