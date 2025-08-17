20ab6e024e41a0f666b0595ece9e6db4a45b3b32
Flor Arenas <flarenas@correo.univo.edu.mx>
2025-08-16 18:25:21 -0600

    Se creo la rama pilas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Delevop!");
+        //Uso básico de Stack: push, pop, peek - Simulación de Pila LIFO
+
+        
     }
 }
